19a90eb704f5163c404060761ff2eb924637197c
LuisMQA <luis.quiala@hlg.jovenclub.cu>
2020-03-30 12:36:15 -0400

    Trabajo con Rutas

---
+++ resources/views/permiso.blade.php
@@ -0,0 +1,4 @@
+Sistema de permison del sistema {{$nombre , ' '}} {{$slug}}
+<br>
+<a href="{{url('admin/sistema/permiso')}}"> ir a permisos usado helper 'url'</a><br>
+<a href="{{route('permiso')}}"> ir a permisos usado helper 'route'</a> //este metodo es mejor
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,22 @@
 |
 */
 
+use App\Http\Controllers\PermisoController;
+/*
 Route::get('/', function () {
-    return view('welcomes');
+    return view('welcome');
 });
+
+Route::get('foo', function () {
+    return 'Hola';
+});*/
+
+//Route::get('ruta', 'controlador@metodo');          
+//Route::get('permiso', 'PermisoController@index');
+//Route::get('permiso', 'PermisoController@create');
+
+//Route::view('permiso', 'permiso'); //Route::view('rura', 'NombreVista');
+
+//Route::get('permiso/{nombre}/{slug?}', 'PermisoController@index'); //nombre y slug son 2 parametros, slug es opcional.
+
+Route::get('admin/sistema/permiso', 'PermisoController@index')->name('permiso'); //Una ruta con nombre
